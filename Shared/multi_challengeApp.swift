a8d5e63839524ede798470a8de3b5f21eaac8015
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-05-11 11:05:22 -0300

    initial commit

---
+++ Shared/multi_challengeApp.swift
@@ -0,0 +1,20 @@
+//
+//  multi_challengeApp.swift
+//  Shared
+//
+//  Created by Elias Ferreira on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct multi_challengeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
