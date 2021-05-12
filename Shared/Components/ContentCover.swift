9aaed06742ebd474870563878f57d07bf5663c38
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-12 11:24:27 -0300

    initial gradient structure

--- Shared/Components/ContentCover.swift
+++ Shared/Components/ContentCover.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct ContentCover: View {
+    var title: String
+    var width = CGFloat(162)
+    var height = CGFloat(150)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(content: {
+            Rectangle().fill(Color.red).cornerRadius(14)
+            AuxiliarView(width: width, height: height)
+                .fill(Color.blue).cornerRadius(14)
+
+            Text(title).foregroundColor(.white)
+        })
+        .frame(width: width, height: height, alignment: .center)
+        
+    }
+}
+
+struct AuxiliarView: Shape {
+    var width: CGFloat
+    var height: CGFloat
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.maxX, y: height * 0.3))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - width * 0.3))
+        return path
     }
 }
 
 struct ContentCover_Previews: PreviewProvider {
     static var previews: some View {
-        ContentCover()
+        ContentCover(title: "QuickSort")
     }
 }
