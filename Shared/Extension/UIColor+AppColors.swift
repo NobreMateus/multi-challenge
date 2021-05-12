caacd6c90d7081b87886eab069a7361b1bc4b9d0
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-05-12 10:47:39 -0300

    Color extension created

---
+++ Shared/Extension/UIColor+AppColors.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor.swift
+//  multi-challenge
+//
+//  Created by Elias Ferreira on 12/05/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let dvBlue        = UIColor(named: "DV_Blue") ?? .systemBlue
+    static let dvNavyBlue    = UIColor(named: "DV_NavyBlue") ?? .systemBlue
+    static let dvPurple      = UIColor(named: "DV_Purple") ?? .systemPurple
+    static let dvLightPurple = UIColor(named: "DV_LightPurple") ?? .systemPurple
+    static let dvMagenta     = UIColor(named: "DV_Magenta") ?? .magenta
+    static let dvRed         = UIColor(named: "DV_Red") ?? .systemRed
+    static let dvOrange      = UIColor(named: "DV_Orange") ?? .systemOrange
+    static let dvYellow      = UIColor(named: "DV_Yellow") ?? .systemYellow
+}
