3af69d80e442e8adbd2ec5bd600822d64be6750b
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-05-16 17:16:12 -0300

    Add File

---
+++ Shared/Components/UserRoundedImage.swift
@@ -0,0 +1,20 @@
+//
+//  UserRoundedImage.swift
+//  multi-challenge
+//
+//  Created by Elias Ferreira on 13/05/21.
+//
+
+import SwiftUI
+
+struct UserRoundedImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserRoundedImage_Previews: PreviewProvider {
+    static var previews: some View {
+        UserRoundedImage()
+    }
+}
