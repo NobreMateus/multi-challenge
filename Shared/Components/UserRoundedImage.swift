347488bc45709edb999e9feef671f18c614d0764
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-05-16 20:47:55 -0300

    User rounded image finished

--- Shared/Components/UserRoundedImage.swift
+++ Shared/Components/UserRoundedImage.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct UserRoundedImage: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    var size: CGFloat
+    @State var image: UIImage
 
-struct UserRoundedImage_Previews: PreviewProvider {
-    static var previews: some View {
-        UserRoundedImage()
+    var body: some View {
+        ZStack {
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: size, height: size)
+                .mask(Circle())
+            ZStack {
+                Circle()
+                    .strokeBorder(Color.white, lineWidth: size * 0.06)
+                    .background(Circle().foregroundColor(.dvRed))
+                    .frame(width: size * 0.7, height: size * 0.7)
+                Image(systemName: "xmark")
+                    .foregroundColor(.white)
+                    .font(.system(size: size * 0.32, weight: .bold))
+            }
+            .offset(x: size * 0.5, y: -size * 0.5)
+        }
+        .onTapGesture {
+            // Do Something
+            print("teste")
+        }
     }
 }
