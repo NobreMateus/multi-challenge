02f46ee81a49a706644a1a466d5ff389a8ed09bd
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-05-18 08:57:48 -0300

    File created

---
+++ Shared/MyBinderView.swift
@@ -0,0 +1,21 @@
+//
+//  MyBinderView.swift
+//  multi-challenge
+//
+//  Created by Elias Ferreira on 18/05/21.
+//
+
+import SwiftUI
+
+struct MyBinderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+    }
+}
+
+struct MyBinderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyBinderView()
+    }
+}
