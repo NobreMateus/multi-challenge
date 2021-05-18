4cc1f2d22d138aca545dc44477d42e2bd5ab6a5e
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-18 11:27:23 -0300

    new color

---
+++ Shared/Components/ExDivider.swift
@@ -0,0 +1,20 @@
+//
+//  ExDivider.swift
+//  multi-challenge (iOS)
+//
+//  Created by Lidiane Gomes Barbosa on 18/05/21.
+//
+
+import SwiftUI
+
+struct ExDivider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExDivider_Previews: PreviewProvider {
+    static var previews: some View {
+        ExDivider()
+    }
+}
