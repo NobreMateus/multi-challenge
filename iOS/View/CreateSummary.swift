fe41e6cd76770b875aa240effedb46e6f0c26a7a
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-19 09:14:13 -0300

    create summary

---
+++ iOS/View/CreateSummary.swift
@@ -0,0 +1,23 @@
+//
+//  CreateSummary.swift
+//  multi-challenge (iOS)
+//
+//  Created by Lidiane Gomes Barbosa on 19/05/21.
+//
+
+import SwiftUI
+
+struct CreateSummary: View {
+    var body: some View {
+        NavigationView {
+                
+        }
+        
+    }
+}
+
+struct CreateSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateSummary()
+    }
+}
