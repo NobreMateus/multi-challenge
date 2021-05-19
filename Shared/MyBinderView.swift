d43adb46ff85ccf0f7037e43194132316126738f
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-05-19 08:59:33 -0300

    Created navbar

--- Shared/MyBinderView.swift
+++ Shared/MyBinderView.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct MyBinderView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
 
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("teste")
+                ContentCoverList(isSharedContent: true, isEditMode: false)
+                    .navigationBarTitle("Meu Fichário", displayMode: .large)
+                    //.navigationBarItems(trailing: Image(systemName: "plus").foregroundColor(.dvLightPurple))
+                    .toolbar { // <2>
+                        ToolbarItem(placement: .navigationBarTrailing) { // <3>
+                            VStack {
+                                Button(action: {}, label: {
+                                    Image(systemName: "plus").foregroundColor(.dvLightPurple)
+                                        .font(.system(size: 24))
+                                        .padding(.top)
+                                })
+                                UserRoundedImage(size: 34, image: UIImage(named: "original")!, showButton: false)
+                                    .offset(x: 0, y: 24)
+                            }
+                        }
+                    }
+            }
+        }
     }
+
 }
 
 struct MyBinderView_Previews: PreviewProvider {
