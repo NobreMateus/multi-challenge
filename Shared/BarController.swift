189416eebd01b93f05673fca82f3490742ea58dc
Eduardo Regis <eduregis1@gmail.com>
2021-05-20 09:24:36 -0300

    call the correct screen

--- Shared/BarController.swift
+++ Shared/BarController.swift
@@ -46,7 +46,7 @@ struct BarController: View {
                         Image(systemName: "doc.text")
                         Text("Meu Fichário")
                     }
-                CopyTo()
+                Groups()
                     .environment(\.managedObjectContext, persistenceController.container.viewContext)
                     .tag(1)
                     .tabItem {
