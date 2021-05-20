7b739b86172c4ff9ecd37743ad89ea1ee6aeb4de
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-20 11:50:05 -0300

    view for create summary

--- iOS/View/CreateSummary.swift
+++ iOS/View/CreateSummary.swift
@@ -8,16 +8,71 @@
 import SwiftUI
 
 struct CreateSummary: View {
+    @Binding var title: String
+    @Binding var textBody: String
+    var size: CGSize
+
     var body: some View {
-        NavigationView {
-                
-        }
+        Divider()
         
+        VStack {
+            VStack {
+                Text("Titulo do resumo:")
+                    .font(.body)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                TextField("Adicione um título", text: $title)
+                    .accentColor(.dvLightPurple)
+                    .keyboardType(.default)
+                    .padding(8)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.3),
+                                        lineWidth: 1), alignment: .center)
+                
+            }
+            .onTapGesture {
+                self.hideKeyboard()
+            }
+            .padding()
+            
+            Divider()
+            
+            Text("\(textBody.count) caracteres")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.trailing)
+            
+            ZStack(alignment: .topLeading) {
+                
+                if textBody.isEmpty {
+                    Text("Escreva seu texto aqui")
+                        .foregroundColor(Color(UIColor.placeholderText))
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 12)
+                        .showView(textBody.isEmpty)
+                }
+                
+                TextEditor(text: $textBody)
+                    .padding(4)
+                    .frame(minWidth: size.width * 0.95, minHeight: size.height * 0.5, alignment: .center)
+                    
+                    .font(.body)
+                    .clipped()
+                
+            }.onTapGesture {
+                self.hideKeyboard()
+            }
+        }
     }
 }
 
-struct CreateSummary_Previews: PreviewProvider {
-    static var previews: some View {
-        CreateSummary()
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
+#endif
