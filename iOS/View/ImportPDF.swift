3504af30b636e8e13228cd63c4f66c9b9097ba71
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-20 11:50:40 -0300

    view for import pdf

--- iOS/View/ImportPDF.swift
+++ iOS/View/ImportPDF.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct ImportPDF: View {
+    @State private var fileContent = ""
+    @State private var showDocumentPicker = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            HStack {
+                
+                Button(action: {
+                    showDocumentPicker = true
+                }, label: {
+                    Label("Escolher PDF", systemImage: "arrow.up.square")
+                        .foregroundColor(.white)
+                })
+                .padding(12)
+                .frame(width: geo.size.width * 0.5, alignment: .center)
+                .background(
+                    
+                    RoundedCorners(color: .dvLightPurple, topLeft: 0, topRight: 10, bottomLeft: 0, bottomRight: 10)
+                )
+                
+                .sheet(isPresented: $showDocumentPicker) {
+                    DocumentPicker(fileContent: $fileContent)
+                }
+                
+                if fileContent.isEmpty {
+                    Text("Sem arquivo ainda...")
+                        .padding(12)
+                        .opacity(0.3)
+                        .frame(width: geo.size.width * 0.5, alignment: .center)
+                        
+                } else {
+                    Text(fileContent)
+                        .padding(12)
+                        .frame(width: geo.size.width * 0.5, alignment: .center)
+                }
+            }.overlay(
+                Rectangle()
+                    .stroke(Color.gray, lineWidth: 1)
+                    .opacity(0.3),
+                alignment: .center
+            )
+        }
     }
 }
 
