003b7f29e84fc06a3d734f6f35a180955402065c
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-20 11:48:54 -0300

    view for upload file

--- Shared/Components/DocumentPicker.swift
+++ Shared/Components/DocumentPicker.swift
@@ -5,4 +5,38 @@
 //  Created by Lidiane Gomes Barbosa on 20/05/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct DocumentPicker: UIViewControllerRepresentable {
+    
+    @Binding var fileContent: String
+    
+    func makeCoordinator() -> DocumentPickerCoordinator {
+        return DocumentPickerCoordinator(fileContent: $fileContent)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+        
+        return UIDocumentPickerViewController(forOpeningContentTypes: [.pdf], asCopy: true)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {
+    }
+}
+
+class DocumentPickerCoordinator: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate {
+    @Binding var fileContent: String
+    
+    init(fileContent: Binding<String>) {
+        _fileContent = fileContent
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        let fileURL = urls[0]
+        do {
+            fileContent = try String(contentsOf: fileURL, encoding: .utf8)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
