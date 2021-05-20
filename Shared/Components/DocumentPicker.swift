003d95852f2e687b39322a61f399c5fab34970dd
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-20 11:48:29 -0300

    auxiliar views

---
+++ Shared/Components/DocumentPicker.swift
@@ -0,0 +1,8 @@
+//
+//  DocumentPicker.swift
+//  multi-challenge
+//
+//  Created by Lidiane Gomes Barbosa on 20/05/21.
+//
+
+import Foundation
---
+++ Shared/Components/RoundedCorners.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedCorners.swift
+//  multi-challenge (iOS)
+//
+//  Created by Lidiane Gomes Barbosa on 20/05/21.
+//
+
+import SwiftUI
+
+struct RoundedCorners: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoundedCorners_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedCorners()
+    }
+}
