b1dd39b313cc9de836ac2a85b2ba26d3287519d1
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-20 11:50:26 -0300

    view for add material

--- iOS/View/AddMaterial.swift
+++ iOS/View/AddMaterial.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct AddMaterial: View {
+    @State private var selection = 0
+    @State private var title = ""
+    @State private var textBody = ""
+    
+    init() {
+        UITextView.appearance().backgroundColor = .clear
+        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(Color.dvLightPurple)
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            GeometryReader { geo in
+                ScrollView {
+                    VStack {
+                        VStack {
+                            
+                            Picker(selection: $selection, label: Text("")) {
+                                Text("Escrever resumo").bold().tag(0)
+                                    
+                                Text("Importar PDF").tag(1)
+                            }
+                            .frame(height: 60, alignment: .center)
+                            .pickerStyle(SegmentedPickerStyle())
+                            .scaleEffect(CGSize(width: 1, height: 1.3))
+                            .listItemTint(Color.dvLightPurple)
+
+                        }.padding()
+                        
+                        if selection == 0 {
+                            CreateSummary(title: $title, textBody: $textBody, size: geo.size)
+                            
+                        } else if selection == 1 {
+                            ImportPDF()
+                        }
+                        
+                    }
+                }
+                
+            }
+            
+            .navigationBarTitle("Adicionar Material", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Concluir", action: {
+                print(title)
+                print(textBody)
+            }))
+        }
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
 
