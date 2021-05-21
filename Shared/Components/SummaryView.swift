f1c00331447446bc1b404aaa433764183dd3020e
Mateus <mateusnobref@gmail.com>
2021-05-21 11:08:40 -0300

    Visualizador de resumo criado

---
+++ Shared/Components/SummaryView.swift
@@ -0,0 +1,56 @@
+//
+//  SummaryView.swift
+//  multi-challenge
+//
+//  Created by Mateus Nobre on 21/05/21.
+//
+//swiftlint:disable force_try
+
+import SwiftUI
+import HighlightedTextEditor
+
+let betweenUnderscores = try! NSRegularExpression(pattern: "_[^_]+_", options: [])
+let betweenAsterisk = try! NSRegularExpression(pattern: "\\*[^*]+\\*", options: [])
+let betweenDashes = try! NSRegularExpression(pattern: "-[^_]+-", options: [])
+
+struct SummaryView: View {
+    
+    var editMode: Bool
+    @State var text: String = "ALOHA"
+    
+    #if os(iOS)
+    private let rules: [HighlightRule] = [
+        HighlightRule(pattern: betweenAsterisk, formattingRules: [
+            TextFormattingRule(fontTraits: .traitBold)
+        ]),
+        HighlightRule(pattern: betweenDashes, formattingRules: [
+            TextFormattingRule(fontTraits: .traitItalic)
+        ])
+    ]
+    #else
+    private let rules: [HighlightRule] = [
+        HighlightRule(pattern: betweenAsterisk, formattingRules: [
+            TextFormattingRule(fontTraits: .bold)
+        ]),
+        HighlightRule(pattern: betweenDashes, formattingRules: [
+            TextFormattingRule(fontTraits: .italic)
+        ])
+    ]
+    #endif
+    
+    var body: some View {
+        VStack {
+            HighlightedTextEditor(
+                text: $text,
+                highlightRules: rules,
+                editMode: editMode
+            )
+                }
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView(editMode: true)
+    }
+}
