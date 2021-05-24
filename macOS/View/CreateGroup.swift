5aeb4d7a376326778d844c1c352cc7db49466cd0
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-24 09:33:22 -0300

    macos

--- macOS/View/CreateGroup.swift
+++ macOS/View/CreateGroup.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct CreateGroup: View {
     @Binding var isPresented: Bool
+    @State private var groupName: String = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image("CreateGroupIcon")
+                .scaledToFit()
+                .foregroundColor(.dvPurple)
+            Text("Criar grupo")
+                .foregroundColor(.dvPurple)
+            Divider()
+            
+            HStack {
+                Text("Nome do grupo")
+                Spacer()
+                TextField("Dê um nome ao grupo", text: $groupName)
+                    .accentColor(.dvPurple)
+                    .keyboardType(.default)
+                    .padding(8)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(UIColor.separator),
+                                        lineWidth: 1), alignment: .center)
+            }
+            
+            Text("Escolha uma imagem ou cor:")
+            AvatarHandler()
+            
+        }
+       
     }
 }
 //
