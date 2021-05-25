ce1c41650eea9abeb8e59578c46dd8a0b947af3e
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-05-25 09:45:42 -0300

    new files

---
+++ macOS/View/AddMaterial.swift
@@ -0,0 +1,20 @@
+//
+//  AddMaterial.swift
+//  multi-challenge (macOS)
+//
+//  Created by Lidiane Gomes Barbosa on 25/05/21.
+//
+
+import SwiftUI
+
+struct AddMaterial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddMaterial_Previews: PreviewProvider {
+    static var previews: some View {
+        AddMaterial()
+    }
+}
